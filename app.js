65d32348badecdc512db8c347363c1e36b7fbf0d
emilyjiminroh <emilyjiminroh@gmail.com>
2021-08-03 18:23:21 +0900

    using localstorage

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ const loginForm = document.querySelector(".login-form");
 const greeting = document.querySelector(".greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY,username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username){
   greeting.innerText = `Hello ${username} !!!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-
 loginForm.addEventListener("submit", onLoginSubmit);
 // 이 부분에서 함수명()을 안하는 이유
 // -> 이 이벤트가 충족되었을 때만 실행하고 싶기 때문이다! 
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername===null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit",onLoginSubmit);
+} else{
+  paintGreetings(savedUsername);
+}
